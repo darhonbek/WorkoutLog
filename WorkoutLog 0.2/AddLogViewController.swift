69eae987423f156c6fd702c319b3262702502e12
Darkhonbek <darhonbek@gmail.com>
2017-11-03 16:13:50 +0500

    - Minor UX fixes:     - Done button hides the keyboard     - Text on textfields remain on focus

---
+++ WorkoutLog 0.2/AddLogViewController.swift
@@ -0,0 +1,75 @@
+////
+////  AddLogView.swift
+////  Workout Log
+////
+////  Created by Darkhonbek Mamataliev on 3/11/17.
+////  Copyright © 2017 Darkhonbek Mamataliev. All rights reserved.
+////
+//
+//import Foundation
+//import UIKit
+//import CoreData
+//
+//class AddLogViewController: UIViewController {
+//
+//    lazy var muscleTextField: UITextField! = {
+//        let textField = UITextField()
+//        textField.translatesAutoresizingMaskIntoConstraints = false
+//        textField.borderStyle = .RoundedRect
+//        textField.textAlignment = .Center
+//
+//        return textField
+//    }()
+//
+//    lazy var exerciseTextField: UITextField! = {
+//        let textField = UITextField()
+//        textField.translatesAutoresizingMaskIntoConstraints = false
+//        textField.borderStyle = .RoundedRect
+//        textField.textAlignment = .Center
+//
+//        return textField
+//    }()
+//
+//    lazy var repsTextField: UITextField! = {
+//        let textField = UITextField()
+//        textField.translatesAutoresizingMaskIntoConstraints = false
+//        textField.borderStyle = .RoundedRect
+//        textField.textAlignment = .Center
+//        textField.keyboardType = .numberPad
+//
+//        return textField
+//    }()
+//
+//    lazy var weightTextField: UITextField! = {
+//        let textField = UITextField()
+//        textField.translatesAutoresizingMaskIntoConstraints = false
+//        textField.borderStyle = .RoundedRect
+//        textField.textAlignment = .Center
+//        textField.keyboardType = .numberPad
+//
+//        return textField
+//    }()
+//
+//    lazy var muslceLabel: UILabel! = {
+//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 150, height: 25))
+//        label.center = CGPoint(x: 160, y: 285)
+//        label.textAlignment = .center
+//        label.text = "Muscle:"
+//
+//    }()
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        view.addSubview(muscleTextField)
+//        view.addSubview(exerciseTextField)
+//        view.addSubview(repsTextField)
+//        view.addSubview(weightTextField)
+//
+//
+//    }
+//
+//
+//}
+
